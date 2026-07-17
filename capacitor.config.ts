69b5c4b350e6215eca47f98fb9424017acd91c44
NotKT <notkt8130@gmail.com>
2026-07-17 20:56:09 +0530

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.nitrogrid.app',
-  appName: 'Nitro-K',
-  webDir: 'www'
-};
-
-export default config;
