ef123f51f689414856c0adf0c83b8a668771325a
NotKT <notkt8130@gmail.com>
2026-07-17 20:41:20 +0530

    Add files via upload

---
+++ android/app/src/main/java/com/nitrogrid/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nitrogrid.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nitrogrid.app',
+  appName: 'Nitro-K',
+  webDir: 'www'
+};
+
+export default config;
